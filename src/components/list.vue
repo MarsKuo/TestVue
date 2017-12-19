947d7596c931a3c68b5e2f3bfc5fc42dbf136053
Mars <talent.seek@msa.hinet.net>
2017-12-19 18:11:10 +0800

    2nd

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,9 +1,10 @@
 // HTML
 <template>
 <div class="view">
-  {{
-    title
-  }}
+  <h1>{{title}}</h1>
+  <ul>
+    <li v-for="item in items" :key=item.index1 :class="{finish:item.isFinished}">{{item.text}}</li>
+  </ul>
 </div>
 </template>
 
@@ -18,7 +19,17 @@ export default {
   // }
   data() {
     return {
-      title: "TodoList"
+      title: "TodoList",
+      items: [
+        {
+          text: "今天晚上18:00跑步",
+          isFinished: false
+        },
+        {
+          text: "今天早餐",
+          isFinished: true
+        }
+      ]
     };
   }
 };
@@ -29,8 +40,14 @@ export default {
   width: 400px;
   border: 1px solid #000;
   margin: 20px auto;
-  background-color: pink;
+  /* background-color: pink; */
   padding: 20px;
+  font-weight: 900;
+  /* font-size: 40px; */
+}
+.finish{
+  color: grey;
+  text-decoration: line-through;
 }
 </style>
 
