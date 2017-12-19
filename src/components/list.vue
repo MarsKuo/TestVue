f7a10deac97f61dd9a5af2021cb5b3a06d5581c9
Mars <talent.seek@msa.hinet.net>
2017-12-19 17:40:15 +0800

    '1st'

---
+++ src/components/list.vue
@@ -0,0 +1,36 @@
+// HTML
+<template>
+<div class="view">
+  {{
+    title
+  }}
+</div>
+</template>
+
+// JS
+<script>
+export default {
+  name: "list",
+  // data: function(){
+  //   return {
+
+  //   }
+  // }
+  data() {
+    return {
+      title: "TodoList"
+    };
+  }
+};
+</script>
+
+<style>
+.view {
+  width: 400px;
+  border: 1px solid #000;
+  margin: 20px auto;
+  background-color: pink;
+  padding: 20px;
+}
+</style>
+
